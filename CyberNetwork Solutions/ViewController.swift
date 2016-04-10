30a161073efe0dd5b9116fe8a2424caaf1c223ab
AHM.Sajid <ahmsajid360@gmail.com>
2016-04-10 18:53:05 -0400

    Initial Commit

---
+++ CyberNetwork Solutions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CyberNetwork Solutions
+//
+//  Created by AHM. SAJID on 4/10/16.
+//  Copyright © 2016 Sabilopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
